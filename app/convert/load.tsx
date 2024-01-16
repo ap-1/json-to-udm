44da993783ccff7f66a87698c08a23f7cd74e25e
Anish Pallati <anishp@duck.com>
2024-01-16 11:07:47 -0500

    fix: remove extraneous import

--- app/convert/load.tsx
+++ app/convert/load.tsx
@@ -3,7 +3,6 @@ import { useState, type MouseEventHandler, ChangeEventHandler } from "react";
 import { toast } from "sonner";
 import { gunzipSync } from "zlib";
 
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
